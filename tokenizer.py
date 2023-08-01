c65fd549407248cc51ecab9ddeaca615b73ab96b
Emil Nuutinen <emil@emilnuutinen.com>
2023-08-01 11:29:49 +0300

    New tokenizer and config

--- tokenizer.py
+++ tokenizer.py
@@ -1,16 +1,15 @@
-from pathlib import Path
-
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.trainers import BpeTrainer
 
-paths = [str(x) for x in Path("./fi_corpus/").glob("**/*.txt")]
+path = ["data/all.txt"]
 
 tokenizer = Tokenizer(BPE())
 tokenizer.pre_tokenizer = Whitespace()
 trainer = BpeTrainer(
-    special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"])
-tokenizer.train(files=paths, trainer=trainer)
+    special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"],
+    show_progress=True)
+tokenizer.train(files=path, trainer=trainer)
 
 tokenizer.save("model/tokenizer.json")
