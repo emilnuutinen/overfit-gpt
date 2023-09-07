72383957c1048d1b41489d0fce648338a6e17aa3
Emil Nuutinen <emil@emilnuutinen.com>
2023-09-07 16:25:17 +0300

    Clean up unused scripts

--- scripts/continue_train.sh
+++ scripts/continue_train.sh
@@ -14,19 +14,25 @@ pip3 install -r requirements.txt
 srun python3 train.py \
   --overwrite_cache \
   --model_type gpt2 \
-  --tokenizer_name Finnish-NLP/gpt2-finnish \
+  --tokenizer_name TurkuNLP/gpt3-finnish-small \
+  --config_overrides="bos_token_id=1,eos_token_id=2,vocab_size=131072" \
   --cache_dir cache/ \
-  --train_file data/train.txt \
-  --validation_file data/dev.txt \
-  --per_device_train_batch_size 1 \
-  --per_device_eval_batch_size 1 \
+  --dataset_name graelo/wikipedia \
+  --dataset_config_name 20230601.fi \
+  --preprocessing_num_workers 4 \
+  --per_device_train_batch_size 2 \
+  --per_device_eval_batch_size 2 \
+  --gradient_accumulation_steps 16 \
+  --gradient_checkpointing True \
+  --block_size 512 \
+  --optim adamw_torch \
   --learning_rate 4e-5 \
   --do_train \
   --do_eval \
   --evaluation_strategy steps \
-  --eval_steps 10000 \
+  --eval_steps 50000 \
   --output_dir tmp/ \
   --save_steps 10000 \
-  --num_train_epochs 50 \
+  --num_train_epochs 200 \
   --save_total_limit 5 \
-  --resume_from_checkpoint tmp/checkpoint-450000
+  --resume_from_checkpoint tmp/checkpoint-50000
