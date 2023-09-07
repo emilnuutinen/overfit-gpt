e2205f696650fa392fbf1fb278176fd4d6da3575
Emil Nuutinen <emil@emilnuutinen.com>
2023-09-07 15:34:55 +0300

    Fix bugs

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -15,13 +15,16 @@ srun python3 train.py \
   --overwrite_cache \
   --model_type gpt2 \
   --tokenizer_name TurkuNLP/gpt3-finnish-small \
-  --config_overrides="vocab_size=131072" \
+  --config_overrides="bos_token_id=1,eos_token_id=2,vocab_size=131072" \
   --cache_dir cache/ \
   --dataset_name graelo/wikipedia \
   --dataset_config_name 20230601.fi \
-  --streaming True \
-  --per_device_train_batch_size 4 \
-  --per_device_eval_batch_size 4 \
+  --preprocessing_num_workers 4 \
+  --per_device_train_batch_size 2 \
+  --per_device_eval_batch_size 2 \
+  --gradient_accumulation_steps 16 \
+  --gradient_checkpointing True \
+  --block_size 512 \
   --learning_rate 4e-5 \
   --do_train \
   --do_eval \
