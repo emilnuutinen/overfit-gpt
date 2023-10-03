841e90c50e41c90958edcd32d9017e18d50a40c3
Emil Nuutinen <emil@emilnuutinen.com>
2023-10-03 15:36:44 +0300

    Update test setup

--- test_models.py
+++ test_models.py
@@ -1,27 +1,44 @@
 from nltk.translate.bleu_score import sentence_bleu
-from transformers import pipeline, set_seed
+from transformers import BloomTokenizerFast, pipeline, set_seed
 
-first = pipeline('text-generation', model='checkpoints/1208_150k_steps/')
-second = pipeline('text-generation', model='checkpoints/1408_300k_steps/')
-third = pipeline('text-generation', model='checkpoints/2108_450k_steps/')
+tokenizer = BloomTokenizerFast.from_pretrained("TurkuNLP/gpt3-finnish-small")
+first = pipeline('text-generation', model='tmp_dropout/checkpoint-210000')
+second = pipeline('text-generation', model='tmp_mini')
 
-set_seed(42)
+set_seed(0)
 
-prompt = "Amsterdam on Alankomaiden pääkaupunki. Amsterdam on väkiluvultaan Alankomaiden suurin kaupunki. Vuonna 2022 "
+prompt = "Selm on kaupunki Nordrhein-Westfalenin osavaltiossa läntisessä Saksassa."
 
-real = """Amsterdam on Alankomaiden pääkaupunki. Amsterdam on väkiluvultaan Alankomaiden suurin kaupunki. Vuonna 2022 siellä asui 910 146 asukasta eli noin joka 20. hollantilainen asuu Amsterdamissa. Yhteensä Amsterdamissa ja sitä ympäröivällä asuu noin 550 000 ihmistä eli vajaa kymmenesosa Alankomaiden asukkaista. Vaikka Amsterdam on Alankomaiden perustuslain mukaan maan pääkaupunki, sijaitsevat niin kuningashuone, hallitus, parlamentti kuin korkein oikeuskin sekä ulkomaiden diplomaattiset edustustot Haagissa. Amstel-joen suistoon 1100-luvulta alkaen rakennettu ja 1300-luvulta alkaen nopeasti kasvanut Amsterdam tunnetaan vanhoista taloistaan ja kauniista kanaaleistaan. Keskustan vanhat, puupaalujen varaan rakennetut rakennukset on suojeltu. Aikoinaan kaupungilla on ollut merkittävä rooli Itämeren, Pohjanmeren ja Välimeren välisessä Noin seitsemän metriä merenpinnan alapuolella sijaitseva Amsterdamin lentokenttä Schiphol on merkittävä keskus ja Euroopan toiseksi suurin. Amsterdam on kuuluisa ja kaupungista onkin muodostunut eurooppalaisen vertauskuva. Punaisten lyhtyjen alue, myyvät coffee shopit ja monipuolinen yöelämä vetävät yhdessä kauniin arkkitehtuurin kanssa puoleensa runsaasti turisteja.  Maantiede ja ilmasto  Amsterdam sijaitsee Amstelin suistossa IJsselmeerin rannalla Alankomaiden provinssissa. Kaupunki on tasaisella alankoalueella, ja osa siitä on merenpinnan tason alapuolella. Kaupunki on IJsselmeeriin kuuluvan IJ’n etelärannalla. Amsteljoki virtaa kaupungin läpi. Amsterdamissa vallitsee lauhkea meri-ilmasto. Sen läheisyys Pohjanmereen vaikuttaa voimakkaasti säätiloihin. Talvet ovat leutoja, kylmimpien kuukausien (tammi- ja helmikuun) keskimääräinen alin lämpötila on hiukan plussan puolella. Toisinaan sataa hiukan lunta ja joskus harvoin kanavat jäätyvät ja niillä voi luiustella. Kesät ovat yleensä lämpimiä, harvoin kuumia, heinä'- ja elokuun keskimääräinen ylin lämpötila on 20 ja 25 asteen välillä."""
+paragraph = """Selm on kaupunki Nordrhein-Westfalenin osavaltiossa läntisessä Saksassa. Se sijaitsee historiallisen Münsterlandin eteläosassa. Ensimmäinen kirjallinen maininta Selmista on vuodelta 858 ja kaupunkioikeudet se sai 1977. Maantiede Selm sijaitsee Ruhrin alueen koillisosassa ja piirikuntansa luoteiskolkassa. Enintään 50 kilometrin säteellä kaupungin keskustasta sijaitsee useita suurkaupunkeja, kuten Dortmund etelässä. Etäisyys Kölnistä on noin 90 kilometriä koilliseen ja matka Kölnistä on maanteitse noin 120 kilometriä. Selmin viralliset kaupunginosat ovat Selm, Bork ja Cappenberg. Selmin lounaisrajana on Lippe-joki, joka oli aikoinaan Münsterin hiippakunnan eteläraja. Historia Nykyisen Selmin alueella on arkeologisten löytöjen perusteella asuttu jo neoliittisella kaudella. Aikakirjoihin Selm ilmaantuu vuonna 858 muodossa Seliheim. Selm oli 1900-luvun alkuun saakka ilmeeltään täysin maalainen. Kaivos Zeche Hermann aloitti toimintansa 1909, se vaati runsaasti työvoimaa ja paikkakunnan väkimäärä viisinkertaistui 2 000:sta. Kaivoksen sulkeminen 1926 teki Selmista kriisikunnan: se oli niin sanottu Notstandsgemeinde vuodesta 1934 vuoteen 1956. Nykyinen Selm syntyi 1975, kun Selm ja sen eteläpuolinen Bork liitettiin toisiinsa. Borkin kirjoitettu historia alkaa 800-luvulta. Selm sai kaupunkioikeudet 1977. Selmin historiallisia rakennuksia ovat muun muassa keskiaikaiset Friedenskirche ja Burg Botzlar. Borkissa sijaitsee kirkko St. Stephanus, jonka sipulikupoli edustaa seutukunnalle epätyypillistä arkkitehtuuria. Kaupungin kaakkoisosassa sijaitsee Schloss Cappenberg. Talous ja liikenne Selmissa sijaitsee kansainvälisen Rethmann-konsernin pääkonttori. Kaupungin teollisuusalueet sijaitsevat kantakaupungin itäpuolella ja Borkin pohjoispuolella. Selmissa on kolme rautatieliikennepaikkaa: Selm, Selm-Beifang ja Selm-Bork. Ne sijaitsevat rataosuudella Dortmund–Enschede. Selmin keskeinen maantieliikenteen väylä on Bundesstraße 236, josta on yhteys kaupungin eteläpuolitse kulkevaan moottoritiehen BAB 2. Selmin itäpuolitse kulkee BAB 1.
+"""
 
-first = first(prompt, max_new_tokens=100, num_return_sequences=1)[
-    0]['generated_text'].split()
+tokens = tokenizer.tokenize(paragraph)
+reference = tokens[:300]
+bleu_score = sentence_bleu([reference], reference)
+
+print()
+print("Base text:")
+print()
+print(tokenizer.convert_tokens_to_string(reference))
+print("BLEU Score:", bleu_score)
+print()
+print()
+
+prompt_tokens = tokenizer.tokenize(prompt)
+min_tokens = 300-len(prompt_tokens)
+
+first = " ".join(first(prompt, min_new_tokens=min_tokens,
+                 max_new_tokens=min_tokens, temperature=0.1)[0]['generated_text'].split())
 print(first)
-print(sentence_bleu(first, real))
+first = tokenizer.tokenize(first)
+bleu_score = sentence_bleu([first], reference)
+print("BLEU Score:", bleu_score)
+print()
 
-second = second(prompt, max_new_tokens=100, num_return_sequences=1)[
-    0]['generated_text'].split()
+second = " ".join(second(prompt, min_new_tokens=min_tokens,
+                  max_new_tokens=min_tokens, temperature=0.1)[0]['generated_text'].split())
 print(second)
-print(sentence_bleu(second, real))
-
-third = third(prompt, max_new_tokens=100, num_return_sequences=1)[
-    0]['generated_text'].split()
-print(third)
-print(sentence_bleu(third, real))
+second = tokenizer.tokenize(second)
+bleu_score = sentence_bleu([second], reference)
+print("BLEU Score:", bleu_score)
+print()
