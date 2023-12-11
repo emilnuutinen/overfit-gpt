ff5c4599b2ce6871c24c34d6f995ba4cae66fdab
Emil Nuutinen <emil@emilnuutinen.com>
2023-12-11 11:57:45 +0200

    Update slurm scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=overfit_gpt
+#SBATCH --job-name=mini
 #SBATCH --account=Project_2002820
 #SBATCH --time=36:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
 #SBATCH --partition=gpumedium
 #SBATCH --gres=gpu:a100:4
 
@@ -14,21 +15,22 @@ pip3 install -r requirements.txt
 srun python3 train.py \
   --overwrite_cache \
   --model_type gpt2 \
-  --tokenizer_name Finnish-NLP/gpt2-finnish \
+  --tokenizer_name TurkuNLP/gpt3-finnish-small \
+  --config_overrides="vocab_size=131072" \
   --cache_dir cache/ \
   --dataset_name graelo/wikipedia \
   --dataset_config_name 20230601.fi \
-  --preprocessing_num_workers 4 \
-  --per_device_train_batch_size 1 \
-  --per_device_eval_batch_size 1 \
-  --gradient_accumulation_steps 4 \
-  --optim adamw_torch \
+  --per_device_train_batch_size 4 \
+  --per_device_eval_batch_size 4 \
+  --gradient_accumulation_steps 8 \
   --learning_rate 4e-5 \
+  --optim adamw_torch \
   --do_train \
   --do_eval \
   --evaluation_strategy steps \
-  --eval_steps 40000 \
-  --output_dir tmp/ \
-  --save_steps 10000 \
+  --eval_steps 20000 \
+  --output_dir checkpoints \
+  --save_steps 5000 \
   --num_train_epochs 200 \
   --save_total_limit 5 \
+  --overwrite_output_dir
